e4f6b9c5be6da0962114a6158e0778f976b2155c
Danish Ehsan <danish.ehsan321@gmail.com>
2020-06-29 13:43:08 -0400

    Implemented slide carousel on posts archive page

--- archive.php
+++ archive.php
@@ -26,6 +26,17 @@ get_header();
                                 <h1 class="post__title"><a href="<?php the_permalink(); ?>" class="post__title-link"><?php the_title(); ?></a></h1>
                                 <p class="post__author">By <a href="<?php echo get_author_posts_url(get_the_author_meta($field = 'ID')); ?>" class="post__author-name"><?php echo get_the_author_meta('display_name'); ?></a><?php edit_post_link('Edit', ' | '); ?></p>
                             </header>
+							<?php 
+								if ( have_rows('post_image_carousel') ) : 	
+							?>
+								<div class="featured__arrows-cont"><i class="fas fa-chevron-left featured__chevron-left"></i><i class="fas fa-chevron-right featured__chevron-right"></i>
+								<div class="post__carousel owl-carousel owl-carousel-post">
+									<?php while ( have_rows('post_image_carousel') ) : the_row(); ?>
+									<div class="post__carousel-image" style="background-image: url(<?php the_sub_field('image'); ?>)"></div>
+									<?php endwhile; ?>
+								</div>
+								</div>
+							<?php endif; ?>
                             <article class="post__content">
                                 <?php the_content(); ?>
                             </article>
